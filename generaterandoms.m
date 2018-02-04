ae3c9ee007a04f767ba8684baf5570237ff8eeb9
Sam Abernethy <abernethy@maths.ox.ac.uk>
2018-02-04 14:43:35 +0000

    Adding seeded random number generator

---
+++ generaterandoms.m
@@ -0,0 +1,13 @@
+function seededrand = generaterandoms(seed)
+% generaterandoms takes a certain seed and constructs a series of random
+% numbers from this seed to maintain consistency
+% INPUTS
+%   seed: integer value that will start the generation
+% OUTPUTS
+%   seededrand: a random number generator made from the original seed
+
+% to use, call rng(seededrand)
+
+seededrand = rng(seed, 'simdTwister');
+
+end
